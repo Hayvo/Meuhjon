8c250fed36ea99a7612ce67b77801ef21647f10d
Hayvo <pierre.denig@mines-paristech.fr>
2021-01-29 10:48:42 +0100

    Fonctions get plateau

---
+++ src/plateau.cpp
@@ -0,0 +1,17 @@
+#include "../include/plateau.h"
+
+int plateau::getNx()
+{
+    return nx;
+}
+
+int plateau::getNy()
+{
+    return ny;
+}
+
+vector<vector<int>> plateau::getBg()
+{
+    return bg;
+}
+
