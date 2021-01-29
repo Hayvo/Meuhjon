d6a6a7da0f11d6df8e66cfc6da318ea206f2ecc0
TonioElbuey <antoine.leboeuf@orange.fr>
2021-01-29 09:46:35 +0100

    Initialisation main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    //Fonction de boucle de jeu
+
+        //Inputs
+
+        //Traitement
+
+        //Actualisation
+
+        //Affichage
+
+        //Test fin de jeu
+
+    return 0;
+}
