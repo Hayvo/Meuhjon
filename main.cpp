112ec79435b5d34a4c50520220b68e084a73c1c7
Amandine Chupin <amandine.chupin@mines-paritech.fr>
2021-01-29 12:50:48 +0100

    Couloir et Pièce

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "./include/plateau.h"
 
+using namespace std;
 int main() {
 
     std::srand(time(0)); //  Attribution d'une seed pour la génération aléatoire
@@ -15,6 +17,19 @@ int main() {
         //Affichage
 
         //Test fin de jeu
+    
+        plateau p;
+
+    p.f_piece( 2, 6, 3, 8);
+    p.f_couloir('h', 5, 8, 12);
+    for (int i = 0 ; i < 20 ; i++)
+    {
+        for (int j = 0 ; j < 20 ; j++)
+        {
+            cout<<p.getCase(i,j);
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
--- src/plateau.cpp
+++ src/plateau.cpp
@@ -1,6 +1,6 @@
 #include "../include/plateau.h"
 
-int plateau::getNx()
+/*int plateau::getNx()
 {
     return nx;
 }
@@ -70,5 +70,61 @@ void plateau::level1()
    piece2.setY1(11);
    piece2.setX2(12);
    piece2.setY2(19);
+}*/
+
+void plateau::f_couloir(char h_or_v, int c, int u1, int u2)
+// h_or_v indique si le couloir est horizontal ou vertical, u1 et u2 le début et la fin du couloir sur la coordonnée dynamique du couloir (y pour horizontale)
+{
+    int hv = h_or_v;
+    int h = 'h';
+    int v = 'v';
+    if (hv = h)
+    {
+        //Portes
+        bg[c][u1] = porte;
+        bg[c][u2] = porte;
+
+        //Couloirs
+        for (int i = u1 + 1 ; i < u2 ; i++)
+        {
+            bg[c][i] = couloir;
+        }
+    }
+    if (hv == v)
+    {
+        //Portes
+        bg[u1][c] = porte;
+        bg[u2][c] = porte;
+
+        //Couloirs
+        for (int i = u1 + 1 ; u1 < u2 ; i++)
+        {
+            bg[i][c] = couloir;
+        }
+    }
 }
 
+void plateau::f_piece(int x_gauche, int x_droit, int y_haut, int y_bas)
+// Les variables d'entrée correspondent aux coins formés par les murs
+{
+    for (int i = x_gauche ; i <= x_droit ; i++)
+    {
+        for (int j = y_haut ; j <= y_bas ; j++)
+        {
+            if (i == x_gauche or i == x_droit or j == y_haut or j == y_bas)
+            {
+                bg[i][j] = mur;
+            }
+            else
+            {
+                bg[i][j] = piece;
+            }
+        }
+    }
+}
+
+int plateau::getCase(int i, int j)
+{
+    int res = bg[i][j];
+    return res;
+}
